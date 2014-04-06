864a6de958ee27b872510a974e1d7b9130b21e08
jacopo.aliprandi@gmail.com <jacopo.aliprandi@gmail.com@6ed44685-d00d-af5a-f5d3-fdcdcd803af9>
2014-04-06 17:24:04 +0000

    proxy pull consumer implemented

--- mtds-corba-demo/src/mtds/alicaldam/eventservice/CosEventChannelAdmin/impl/ProxyPullConsumerImpl.java
+++ mtds-corba-demo/src/mtds/alicaldam/eventservice/CosEventChannelAdmin/impl/ProxyPullConsumerImpl.java
@@ -1,11 +1,16 @@
 package mtds.alicaldam.eventservice.CosEventChannelAdmin.impl;
 
+import org.omg.CORBA.Any;
+import org.omg.CORBA.BooleanHolder;
+
 import mtds.alicaldam.eventservice.CosEventChannelAdmin.AlreadyConnected;
 import mtds.alicaldam.eventservice.CosEventChannelAdmin.ProxyPullConsumerPOA;
 import mtds.alicaldam.eventservice.CosEventChannelAdmin.TypeError;
+import mtds.alicaldam.eventservice.CosEventComm.Disconnected;
 import mtds.alicaldam.eventservice.CosEventComm.PullSupplier;
 
 public class ProxyPullConsumerImpl extends ProxyPullConsumerPOA {
+	private static final int POLLING_INTERVAL_MILLIS = 1000;
 	EventChannelImpl eventChannel;
 	private boolean disconnected = true;
 	private PullSupplier supplier=null;
@@ -23,8 +28,33 @@ public class ProxyPullConsumerImpl extends ProxyPullConsumerPOA {
 		supplier=pull_supplier;
 		disconnected=false;
 		eventChannel.add(this);
-
+		t=new Thread(pullThread);
+		t.start();
+		
 	}
+	
+	private Thread t;
+	private Runnable pullThread = new Runnable() {
+		
+		@Override
+		public void run() {
+			BooleanHolder has_event=new BooleanHolder(false);
+			while(!disconnected){
+				try {
+					Any event=supplier.try_pull(has_event);
+					if(has_event.value==true){
+						eventChannel.supplyEvent(event);
+					}else{
+						Thread.sleep(POLLING_INTERVAL_MILLIS);
+					}
+				} catch (Disconnected | InterruptedException e) {
+					disconnect_pull_consumer();
+				}
+				
+			}
+			
+		}
+	};
 
 	@Override
 	public void disconnect_pull_consumer() {
@@ -39,8 +69,6 @@ public class ProxyPullConsumerImpl extends ProxyPullConsumerPOA {
 			supplier.disconnect_pull_supplier();
 			supplier=null;
 		}
-		
-		
 	}
 
 }
