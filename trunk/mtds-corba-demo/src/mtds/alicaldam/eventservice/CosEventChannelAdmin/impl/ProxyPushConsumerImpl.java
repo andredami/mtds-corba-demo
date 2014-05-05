22237375117b42d66b633ec3ff4319b85c593719
jacopo.aliprandi@gmail.com <jacopo.aliprandi@gmail.com@6ed44685-d00d-af5a-f5d3-fdcdcd803af9>
2014-05-05 09:06:45 +0000

    boh

--- trunk/mtds-corba-demo/src/mtds/alicaldam/eventservice/CosEventChannelAdmin/impl/ProxyPushConsumerImpl.java
+++ trunk/mtds-corba-demo/src/mtds/alicaldam/eventservice/CosEventChannelAdmin/impl/ProxyPushConsumerImpl.java
@@ -1,6 +1,7 @@
 package mtds.alicaldam.eventservice.CosEventChannelAdmin.impl;
 
 import org.omg.CORBA.Any;
+import org.omg.CORBA.OBJECT_NOT_EXIST;
 
 import mtds.alicaldam.eventservice.CosEventChannelAdmin.AlreadyConnected;
 import mtds.alicaldam.eventservice.CosEventChannelAdmin.ProxyPushConsumerPOA;
@@ -8,53 +9,70 @@ import mtds.alicaldam.eventservice.CosEventComm.Disconnected;
 import mtds.alicaldam.eventservice.CosEventComm.PushSupplier;
 
 public class ProxyPushConsumerImpl extends ProxyPushConsumerPOA {
-	private boolean connected=false;
+	private boolean connected = false;
 	EventChannelImpl eventChannel;
 	PushSupplier supplier;
+
 	public ProxyPushConsumerImpl(EventChannelImpl eventChannel) {
-		this.eventChannel=eventChannel;
+		this.eventChannel = eventChannel;
 	}
 
 	@Override
 	public void connect_push_supplier(PushSupplier push_supplier)
 			throws AlreadyConnected {
-		if (this.supplier!=null){
-			throw new AlreadyConnected();
+		synchronized (this) {
+			if (!connected) {
+				if (eventChannel != null) {
+					this.supplier = push_supplier;
+					connected = true;
+				}else{
+					throw new OBJECT_NOT_EXIST();
+				}
+			} else {
+				throw new AlreadyConnected();
+			}
 		}
-		this.supplier=push_supplier;
-		connected=true;
 	}
 
 	@Override
 	public void push(Any data) throws Disconnected {
 		System.out.println("a proxy push consumer: data received!");
-		if (!connected){
-			throw new Disconnected();
+		EventChannelImpl eventChannelTmp = null;
+		synchronized (this) {
+			if (!connected) {
+				throw new Disconnected();
+			}
+			eventChannelTmp = eventChannel;
 		}
 		try {
-			eventChannel.supplyEvent(data);
-		} catch (InterruptedException e) {
+			eventChannelTmp.supplyEvent(data);
+		} catch (InterruptedException e1) {
 			disconnect_push_consumer();
 			throw new Disconnected();
 		}
-
 	}
 
 	@Override
 	public void disconnect_push_consumer() {
+		EventChannelImpl ecTmp = null;
+		PushSupplier sTmp = null;
 
-		connected=false;	
+		synchronized (this) {
+			connected = false;
+			if (eventChannel != null) {
+				eventChannel.remove(this);// TODO think about moving it in tmp
+											// like supplier
+				eventChannel = null;
+			}
+			if (supplier != null) {
+				sTmp = supplier;
+				supplier = null;
+			}
+		}
+		if (sTmp != null) {
+			sTmp.disconnect_push_supplier();
+		}
 
-		if (eventChannel!=null){
-			eventChannel.remove(this);
-			eventChannel=null;
-		}
-	
-		if (supplier!=null){
-			supplier.disconnect_push_supplier();
-			supplier=null;
-		}
-		
 	}
 
 }
