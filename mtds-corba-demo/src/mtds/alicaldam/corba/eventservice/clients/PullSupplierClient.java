5665fc05190982ee4b472448607fe7817b32a21a
andreadamiani90@gmail.com <andreadamiani90@gmail.com@6ed44685-d00d-af5a-f5d3-fdcdcd803af9>
2014-05-08 09:30:55 +0000

    Added Destroy implementation to PullSupplierClient and PushSupplierClient

--- mtds-corba-demo/src/mtds/alicaldam/corba/eventservice/clients/PullSupplierClient.java
+++ mtds-corba-demo/src/mtds/alicaldam/corba/eventservice/clients/PullSupplierClient.java
@@ -17,44 +17,55 @@ import Data.EventHelper;
 
 public class PullSupplierClient {
 
-	public static final String ADDRESS="localhost";
-	public static void main(String args[]){
-		
-		try{
+	public static final String ADDRESS = "localhost";
 
-			ORB orb = ORB.init(args,null);
+	public static void main(String args[]) {
 
-			//create the remote push supplier
-			PullSupplierImpl pull_supplier_impl=new PullSupplierImpl();
-			POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+		try {
+
+			ORB orb = ORB.init(args, null);
+
+			// create the remote push supplier
+			PullSupplierImpl pull_supplier_impl = new PullSupplierImpl();
+			POA rootpoa = POAHelper.narrow(orb
+					.resolve_initial_references("RootPOA"));
 			rootpoa.the_POAManager().activate();
-			org.omg.CORBA.Object ref = rootpoa.servant_to_reference(pull_supplier_impl);
+			org.omg.CORBA.Object ref = rootpoa
+					.servant_to_reference(pull_supplier_impl);
 
-			//take a ref to the event channel
-			org.omg.CORBA.Object sRef = orb.string_to_object("corbaname::"+ADDRESS+"::1050#DemoChannel");
+			// take a ref to the event channel
+			org.omg.CORBA.Object sRef = orb.string_to_object("corbaname::"
+					+ ADDRESS + "::1050#DemoChannel");
 			EventChannel event_channel = EventChannelHelper.narrow(sRef);
-						
-			//connect consumer and supplier
-			SupplierAdmin supplierAdmin=event_channel.for_suppliers();
-			ProxyPullConsumer proxy_pull_consumer=supplierAdmin.obtain_pull_consumer();
+
+			// connect consumer and supplier
+			SupplierAdmin supplierAdmin = event_channel.for_suppliers();
+			ProxyPullConsumer proxy_pull_consumer = supplierAdmin
+					.obtain_pull_consumer();
 			pull_supplier_impl.setPullConsumer(proxy_pull_consumer);
-			proxy_pull_consumer.connect_pull_supplier(pull_supplier_impl._this());
-			
-			//TODO: receive and print data 
-			Scanner scanner= new Scanner(System.in);
-			String dataString=scanner.nextLine();
-			while(!dataString.equalsIgnoreCase("q")){
+			proxy_pull_consumer.connect_pull_supplier(pull_supplier_impl
+					._this());
+
+			// TODO: receive and print data
+			Scanner scanner = new Scanner(System.in);
+			String dataString = scanner.nextLine();
+			while (!dataString.equalsIgnoreCase("q")
+					&& !dataString.equalsIgnoreCase("d")) {
 				System.out.println("sending the message...");
-				Any data= orb.create_any();
-				EventHelper.insert(data, new Event(0,dataString));
+				Any data = orb.create_any();
+				EventHelper.insert(data, new Event(0, dataString));
 				pull_supplier_impl.send(data);
 				System.out.println("message sent!");
-				dataString=scanner.nextLine();
+				dataString = scanner.nextLine();
 			}
-			proxy_pull_consumer.disconnect_pull_consumer();
+			if (dataString.equalsIgnoreCase("d")) {
+				event_channel.destroy();
+			} else {
+				proxy_pull_consumer.disconnect_pull_consumer();
+			}
 			scanner.close();
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
